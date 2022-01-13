80fb2d8f9f62c50911267d4252f0d48b015dc976
노현욱 <rohsikdak@khu.ac.kr>
2022-01-13 23:56:57 +0900

    Add : initialize Project

---
+++ src/test/java/rohsik2/com/carrot/CarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package rohsik2.com.carrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
