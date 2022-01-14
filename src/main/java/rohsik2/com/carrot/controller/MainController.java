078f265adf0fec2c243a9c8808c2c012180a4f2d
노현욱 <rohsikdak@khu.ac.kr>
2022-01-14 12:29:14 +0900

    Add : more urls

--- src/main/java/rohsik2/com/carrot/controller/MainController.java
+++ src/main/java/rohsik2/com/carrot/controller/MainController.java
@@ -1,2 +1,13 @@
-package rohsik2.com.carrot.controller;public class MainController {
+package rohsik2.com.carrot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage(Model model){
+        return "index";
+    }
 }
