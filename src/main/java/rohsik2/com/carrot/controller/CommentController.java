9974a998e84b9d51baa44dc8897568afd64cbfdd
노현욱 <rohsikdak@khu.ac.kr>
2022-04-10 12:18:15 +0900

    Add : swagger-ui for changing this project to backend - frontend structure

--- src/main/java/rohsik2/com/carrot/controller/CommentController.java
+++ src/main/java/rohsik2/com/carrot/controller/CommentController.java
@@ -1,22 +1,41 @@
 package rohsik2.com.carrot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import rohsik2.com.carrot.domain.Comment;
 import rohsik2.com.carrot.domain.Stuff;
 import rohsik2.com.carrot.service.CommentService;
 import rohsik2.com.carrot.service.StuffService;
 
 import java.util.*;
-
+@RestController
 @Controller
 public class CommentController {
     CommentService commentService;
     StuffService stuffService;
+
+
+    @Operation(summary = "test hello", description = "hello api example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@Parameter(description = "이름", required = true, example = "Park") @RequestParam String name) {
+        return ResponseEntity.ok("hello " + name);
+    }
 
     @Autowired
     public void setCommentService(CommentService commentService, StuffService stuffService) {
